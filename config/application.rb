18827ad897b6e7bd49449521a0b74a7473ae8126
Anne Richardson <richardson.ae@gmail.com>
2023-04-10 14:32:25 -0400

    Update config dir files to 7.0.4.3

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Yayme
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -17,10 +17,12 @@ module Yayme
     # in config/environments, which are processed later.
     #
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local # Or :utc
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.active_support.cache_format_version = 7.0
   end
 end
