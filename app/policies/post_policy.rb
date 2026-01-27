13d236d501171816487acf37dcdbf97a3de62f8e
Anne Richardson <richardson.ae@gmail.com>
2026-01-27 15:23:20 -0500

    Add PostPolicy to restrict access to the post show page and other actions

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.joins(:post_type).where(post_types: { user_id: user.id })
+    end
+  end
+
+  def create?
+    user.present?
+  end
+
+  def show?
+    owner_or_admin?
+  end
+
+  def edit?
+    owner_or_admin?
+  end
+
+  def update?
+    owner_or_admin?
+  end
+
+  def destroy?
+    owner_or_admin?
+  end
+
+  private
+
+  def owner_or_admin?
+    record.post_type.user_id == user&.id || user&.admin?
+  end
+end
